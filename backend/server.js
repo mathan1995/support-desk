eca53b21feda5994f3a42bfc563150f7d272ebfe
nikhiljerry29 <nikhil.gupta5667@gmail.com>
2022-04-05 10:18:07 +0530

    Heroku Config

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 const express = require("express");
 const { errorHandler } = require("./middleware/error.middleware");
 const connectDB = require("./config/db");
+const path = require("path");
 
 // Mongo DB connection
 connectDB();
@@ -12,17 +13,27 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to support desk" });
-});
-
+// API Routes
 app.use("/api/users", require("./routes/user.route"));
 app.use("/api/tickets", require("./routes/ticket.route"));
 
-app.get("*", (req, res) => {
-  res.status(404).json({ message: "Error in finding route" });
-});
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+} else {
+  app.get("/", (_, res) => {
+    res.status(200).json({ message: "Welcome to support desk" });
+  });
+
+  app.get("*", (_, res) => {
+    res.status(404).json({ message: "Error in finding route" });
+  });
+}
 app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
